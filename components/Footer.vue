842eeacf991804886793e9c0a5c6e8bbceb45c58
ritacarrilho <ritaclameira@gmail.com>
2023-10-10 11:59:35 +0200

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="footer">
+      <p>&copy; 2023 - One more day at the office</p>
+    </div>
+  </template>
+
+<script>
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+    name: 'Footer',
+    });
+</script>  
+
+<style scoped lang="scss">
+    .footer {
+      background-color: #CDAACE;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 80px;
+      width: 100%;
+    }
+  </style>
